922b0560c5080936cf44193ed55bdd008f083d85
tavi <tavi.nathanson@gmail.com>
2014-05-09 14:05:02 -0400

    Changing a dict to a list, and adding some comments

--- rec/similarity.py
+++ rec/similarity.py
@@ -1,11 +1,15 @@
 from math import sqrt
 
+"""
+A method that returns the euclidean distance between person1 and person2.
+"""
 def sim_distance(prefs, person1, person2):
-  shared_items = {}
+  shared_items = []
   for item in prefs[person1]:
     if item in prefs[person2]:
-      shared_items[item] = 1
+      shared_items.append(item)
 
+  # If there are no shared items, then the distance is 0
   if len(shared_items) == 0: return 0
 
   sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2) for item in shared_items])
