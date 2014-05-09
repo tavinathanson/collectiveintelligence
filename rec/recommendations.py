4ce81485fc9827c8fd87d532c778c5834e9def21
tavi <tavi.nathanson@gmail.com>
2014-05-09 15:10:49 -0400

    Adding a top_matches method

--- rec/recommendations.py
+++ rec/recommendations.py
@@ -1,2 +1,15 @@
-import data
-import similarity
+from data import *
+from similarity import *
+
+"""
+Returns the top n closest people in similarity to the argument person, mapped to
+their similarity scores.
+"""
+def top_matches(prefs, person, n = 5, sim = sim_pearson):
+    # Create a map of people to scores by calling similarity on all other people
+    scores = [(sim(prefs, person, other), other) for other in prefs if other != person]
+
+    # Sort (by key) in decreasing order, and return the top n
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
